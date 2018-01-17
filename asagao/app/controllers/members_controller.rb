903fd7ff76895407fb0d4bee63be71fc513c0e37
Yuki Fujiwara <sky.y.0079@gmail.com>
2018-01-17 03:53:26 +0000

    01/10 Rails 課題

---
+++ asagao/app/controllers/members_controller.rb
@@ -0,0 +1,25 @@
+class MembersController < ApplicationController
+  def index
+  end
+
+  def search
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- asagao/config/routes.rb
+++ asagao/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "top#index"
   get "about" => "top#about", as: "about"
   get "lesson/:action(/:name)" => "lesson"
+  
+  resources :members
 end
